c66703c2d620b0434d80ea024842ad4d1632fc0d
Anna <annank18@gmail.com>
2022-03-11 22:44:43 +0400

    feat: add brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+      const stack = [];
+      const brackets = {};
+
+    bracketsConfig.forEach((b) => (brackets[b[0]] = b[1]));
+
+    const arr = str.split("");
+
+    for (let i = 0; i < arr.length; i++) {
+        if (brackets[stack[stack.length - 1]] === arr[i]) {
+            stack.pop();
+        } else if (brackets.hasOwnProperty(arr[i])) {
+            stack.push(arr[i]);
+        } else {
+            return false;
+        }
+    }
+
+    return stack.length === 0 ? true : false;
 }
